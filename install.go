16b3373dce93ecd5b0f58b77e39e716a13b8a387
cicada <1713856662a@gmail.com>
2020-08-03 22:27:39 +0800

    添加安装时的总入口

--- install.go
+++ install.go
@@ -3,26 +3,62 @@ package main
 import (
 	"fmt"
 	"github.com/Catelemmon/oneKeyMediawiki/checker"
-	"math/rand"
+	"github.com/Catelemmon/oneKeyMediawiki/dockerUtil"
+	"github.com/Catelemmon/oneKeyMediawiki/kernelUtil"
+	"github.com/Catelemmon/oneKeyMediawiki/wikiUtil"
 	"os"
+	"path"
 	"strings"
-	"time"
 )
 
 func main() {
 
 	si := checker.NewSupportInfo()
-	si.GetSupportInfo()
-	si.ShowSupportInfo()
-	fmt.Println("是否需要升级内核以安装Mediawiki?[Y/n]")
-	var updateKernel string
-	fmt.Scanln(&updateKernel)
-	if strings.TrimSpace(updateKernel) == "n" {
-		os.Exit(3)
+	err := si.GetSupportInfo()
+	if err != nil{
+		fmt.Println("获取基础信息失败...")
+		fmt.Printf("%v", err)
+		os.Exit(3 )
 	}
-	fmt.Println("updating kernel...")
-	time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
-	fmt.Println("不支持目标内核")
+	si.ShowSupportInfo()
+	cwd, err := os.Getwd()
+	if err != nil{
+		fmt.Println("获取当前文件夹失败...")
+	}
+	staticsDir := path.Join(cwd, "statics")
+	if si.KernelAllow{
+		// installer docker directly
+		if !si.HasDocker{
+			err := dockerUtil.InstallDocker(path.Join(staticsDir, "docker"))
+			if err != nil{
+				fmt.Printf("安装docker失败，错误信息如下\n%v", err)
+				os.Exit(3)
+			}
+		}
+		fmt.Println("正在装载wiki镜像和启动中....")
+		err = wikiUtil.InstallWiki(path.Join(staticsDir, "images"), path.Join(staticsDir, "scripts"))
+		if err != nil {
+			fmt.Printf("安装wiki镜像失败，错误信息如下\n%v", err)
+			os.Exit(3)
+		}
+	} else{
+		// kernel not allow
+		fmt.Println("是否需要升级内核以安装Mediawiki?[Y/n]")
+		var updateKernel string
+		fmt.Scanln(&updateKernel)
+		if strings.TrimSpace(updateKernel) == "n" {
+			os.Exit(3)
+		}
+
+		fmt.Println("updating kernel...")
+		kernelUtil.InstallKernel(path.Join(staticsDir, "kernel"))
+		fmt.Println("修改内核启动项...")
+		err := kernelUtil.ChangeLaunchOption()
+		if err != nil{
+			fmt.Printf("修改内核启动项失败, 错误信息如下\n%s", err)
+		}
+		fmt.Println("完成内核程序的安装，请重新启动")
+	}
 }
 
 
